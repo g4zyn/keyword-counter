96f0ed3e2ae2a235e935c3049cec0a2381d3c0c6
Marko Gajin <gajin@flocksocial.io>
2022-02-12 21:35:19 +0100

    fix

--- internal/job/controller.go
+++ internal/job/controller.go
@@ -24,11 +24,11 @@ func NewController(channel Channel, poolSize int, scanner Scanner) *Controller {
 // Start
 func (c *Controller) Start(ctx context.Context) {
 	for {
-		go func(ctx context.Context, j <-chan *Job) {
+		go func(j <-chan *Job) {
 			if _, err := c.pool.ProcessCtx(ctx, j); err != nil {
 				log.Printf("failed to process job: %v\n", err)
 				return
 			}
-		}(ctx, c.channel.Stream())
+		}(c.channel.Stream())
 	}
 }
